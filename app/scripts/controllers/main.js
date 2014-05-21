5b841e697ad2ad9006bfcbc46cb7df17f44a4f9d
Zach Schneider <me@zachschneider.com>
2014-05-20 22:50:29 -0500

    making things happen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-angular.module('chuteAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('chuteAngularApp').
+    controller('MainCtrl', ['$scope', 'Chute.API.Asset',
+        function($scope, Asset) {
 
-// Chicago Life       = ayg4tmuc/2514972
-// Chicago Landmarks  = ayg5ofmi/2514973
-// Chicago Food       = ayg6yqbu/2514974
-// Other Chicago      = ayg7ydjb/2514975
+            // Chicago Life       = ayg4tmuc/2514972
+            // Chicago Landmarks  = ayg5ofmi/2514973
+            // Chicago Food       = ayg6yqbu/2514974
+            // Other Chicago      = ayg7ydjb/2514975
 
-angular.module('Test').controller('ChuteCtrl', ['$scope', 'Chute.API.Asset', function($scope, Asset) {
-  $scope.assets = Asset.query({album: 'ayg5ofmi', perPage: 3});
-}]);
+            $scope.assets = Asset.query({
+                album: 'ayg5ofmi',
+                perPage: 0
+            });
+
+
+        }
+    ]);
